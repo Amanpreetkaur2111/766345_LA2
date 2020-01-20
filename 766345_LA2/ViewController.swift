79fb1f827fa949ec6afdc863d3564a8e300e47bc
Amanpreetkaur2111 <aman021011@gmail.com>
2020-01-20 14:11:35 -0500

    Initial Commit

---
+++ 766345_LA2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  766345_LA2
+//
+//  Created by Amanpreet Kaur on 2020-01-20.
+//  Copyright © 2020 Amanpreet Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
